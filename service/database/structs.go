99b176d5e5526156a354dcac9f30bb36a1bb47ac
simonerufo <rufo.1885652@studenti.uniroma1.it>
2023-11-28 11:21:54 +0100

    added new struct.go file

---
+++ service/database/structs.go
@@ -0,0 +1,26 @@
+package database
+
+import "time"
+
+type User struct {
+	UserID   int    `json:"user_id`
+	Username string `json:"username"`
+}
+
+type Post struct {
+	PostID      int       `json:post_id`
+	User        User      `json:"user"`
+	ImageURL    string    `json:"imageURL"`
+	LikesNum    int       `json:"likesNum"`
+	CommentsNum int       `json:"commentsNum"`
+	Timestamp   time.Time `json:"timestamp"`
+}
+
+type Comment struct {
+	CommentID int       `json:"comment_id"`
+	PostID    int       `json:"post_id"`
+	OwnerID   int       `json:"owner_id"`
+	User      User      `json:"user_id"`
+	Text      string    `json:"text"`
+	Timestamp time.Time `json:"timestamp"`
+}
