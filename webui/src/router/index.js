ea4e174dbd9c22f37bca3d9982c8a46849a329e6
simonerufo <simonerufo3@gmail.com>
2024-04-30 10:59:57 +0200

    added first frontend features (login page)

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,7 @@ const router = createRouter({
 		{path: '/link1', component: HomeView},
 		{path: '/link2', component: HomeView},
 		{path: '/some/:id/link', component: HomeView},
+		{path: '/session', component:LoginView}
 	]
 })
 
---
+++ webui/src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+	data: function() {
+		return {
+            username: null,
+		}
+	},
+	methods: {
+        login() {
+
+            console.log("Username: "+this.username);
+        }
+	},
+	mounted() {
+	}
+}
+</script>
+
+<template>
+	<div>
+		<div
+			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<h2 class="h2">Login</h2>
+		</div>
+            
+                <div class="form-group">
+                    <label for="username">Username: </label>
+                    <input type="text" class="form-control" id="username"  placeholder="Enter username" v-model="username">
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary" @click="login">Login</button>
+	</div>
+</template>
+
+<style>
+</style>
