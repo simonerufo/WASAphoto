7a891a13dfacae9353d3b08b9fe31b8051b7443b
simoneru <simonerufo3@gmail.com>
2024-07-16 12:41:53 +0200

    added an header in HomeView

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -4,7 +4,7 @@ export default {
 		return {
 			errormsg: null,
 			loading: false,
-			some_data: null,
+			currentUsername: null,
 		}
 	},
 	methods: {
@@ -21,12 +21,13 @@ export default {
 		},
 	},
 	mounted() {
-		this.refresh()
+		this.refresh();
+		this.currentUsername = localStorage["username"];
 	}
 }
 </script>
 
-<template>
+<!-- <template>
 	<div>
 		<div
 			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -49,6 +50,44 @@ export default {
 		</div>
 
 		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+	</div>
+</template>
+-->
+<template>
+	<div class="stream-page">
+		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<h2>{{ currentUsername }}'s Stream</h2>
+	  </div>
+<!--  
+	  <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+	  <div v-else>
+		<div v-if="stream.length > 0">
+		  <div class="post-container" v-for="post in stream" :key="post.id">
+			<div class="user-info d-flex justify-content-between align-items-center">
+				<div>
+					<RouterLink :to="$pathToProfile(post.username)" class="user-link">
+						<span class="post-name">{{ post.username }}</span>
+					</RouterLink>
+				</div>
+				<div class="post-stats">
+					{{ post.timestamp }}
+					<text class="post-stats-heart"> {{ post.commentCount }} <svg class="feather" style="width: 23px; height: 23px;"><use href="/feather-sprite-v4.29.0.svg#message-circle"/></svg></text> 
+					<text>{{ post.likeCount }} <svg class="feather" style="width: 23px; height: 23px;"><use href="/feather-sprite-v4.29.0.svg#heart"/></svg> </text>
+				</div>
+			</div>
+			<div class="post-content">
+			  <img role="button" :src="post.image" @click="toggleModal(post)" class="post-image"/>
+			  <p v-if="post.caption" class="post-caption">{{ post.caption }}</p>
+			</div>
+		  </div>
+		</div>
+		<div v-else>
+		  <p class="empty-message">There is nothing to display here. Begin by following someone!</p>
+		</div>
+	  </div>
+	</div>
+	<Modal v-if="selectedPost" @close="toggleModal(null)" :photo="selectedPost"></Modal>
+-->	
 	</div>
 </template>
 
