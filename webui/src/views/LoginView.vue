e8e93c9429162f741fc6d4ffaf98f4da9927cc03
simoneru <simonerufo3@gmail.com>
2024-07-18 17:25:48 +0200

    fixed cors bug

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -8,21 +8,23 @@ export default {
 		}
 	},
 	methods: {
-        login() {
+        async login() {
 			try {
                 if (!this.usernameValidation.test(this.username)) throw "Invalid username, it must start with letters and contain only lowercase letters and numbers"
                 if (this.username.length < 3 || this.username.length > 13) throw "Invalid username, it must contains mininum 3 characters and maximum 13 characters"
-                let response = this.$axios.post('/session', {
-                   username: this.username,
+                let response = await this.$axios.post('/session', {
+                   "username": this.username,
                 });
-
+                console.log(response)
 				this.id = response.data;
+                console.log("Id is",this.id);
 				localStorage.uid = this.id;
 				localStorage.username = this.username;
                     
                 this.$router.push("/home");
                 
            } catch (e) {
+                console.log("ciao simo")
                 console.log(e)
                 //document.getElementsByTagName("input")[0].style.outline = "auto";
                 //document.getElementsByTagName("input")[0].style.outlineColor = "red";
